42c19b6fa0134ff9566f33796770e087e4b045f6
Eran Avichzer <eranavichzer@Erans-Mac-mini.local>
2022-08-19 16:44:52 +0300

    more ugly css

--- src/cmps/route-preview.jsx
+++ src/cmps/route-preview.jsx
@@ -10,19 +10,28 @@ export const RoutePreview = ({ route }) => {
 
    const [siri, setSiri] = useState('')
    const [timeRemaining, setTimeRemaining] = useState('')
-   const intervalId = useRef()
+
+   const intervalIdTime = useRef()
+   const intervalIdSiri = useRef()
+
    useEffect(() => {
       loadSiri()
       startIntervral()
 
       return () => {
-         clearInterval(intervalId.current)
+         clearInterval(intervalIdTime.current)
+         clearInterval(intervalIdSiri.current)
       }
    }, [])
 
    const loadSiri = async () => {
       const data = await siriService.query({ stop: route.stop_code, train_no: route.train_no, route_id: route.route_id, direction: route.direction_id })
       setSiri(data[0])
+
+      // intervalIdTime.current = setInterval(() => {
+      //    const data = await siriService.query({ stop: route.stop_code, train_no: route.train_no, route_id: route.route_id, direction: route.direction_id })
+      //    setSiri(data[0])
+      // }, 60000);
    }
 
    const getTime = (date) => {
@@ -44,7 +53,7 @@ export const RoutePreview = ({ route }) => {
 
    const startIntervral = () => {
       getTimeRemainingToArrive()
-      intervalId.current = setInterval(() => {
+      intervalIdTime.current = setInterval(() => {
          getTimeRemainingToArrive()
       }, 60000);
    }
@@ -62,11 +71,19 @@ export const RoutePreview = ({ route }) => {
       }
       else {
          setTimeRemaining('')
-         clearInterval(intervalId.current)
+         clearInterval(intervalIdTime.current)
       }
    }
 
-   console.log('timeRemaining', timeRemaining);
+   const getdays = (day) => {
+      const daysLetters = ['א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ש',]
+      const dayOfWeekDigit = new Date().getDay()
+
+      // if (route.days.charAt(day) === '1' && dayOfWeekDigit === day) return <span className="in-schedule-today">{daysLetters[day]}</span>
+      if (route.days.charAt(day) === '1') return <span className="in-schedule">{daysLetters[day]}</span>
+      // else if (route.days.charAt(day) === '0' && dayOfWeekDigit === day) return <span className="off-schedule-today">{daysLetters[day]}</span>
+      else if (route.days.charAt(day) === '0') return <span className="off-schedule">{daysLetters[day]}</span>
+   }
 
    return (
       <div className="route-preview">
@@ -76,11 +93,22 @@ export const RoutePreview = ({ route }) => {
             <CgArrowLongLeftR />
             <div className="stop-name"><span>לתחנה: </span>{route.stop_name_a}</div>
             <div className="destination-time"><span>בשעה: </span>{route.arrival_time_a.substring(0, 5)}</div>
+
+            <div className="days">
+               <div className="sunday">{getdays(0)}</div>
+               <div className="monday">{getdays(1)}</div>
+               <div className="tuesday">{getdays(2)}</div>
+               <div className="wednesday">{getdays(3)}</div>
+               <div className="thursday">{getdays(4)}</div>
+               <div className="friday">{getdays(5)}</div>
+               <div className="saturday">{getdays(6)}</div>
+            </div>
          </div>
+
 
          <div className="real-time-container">
             {!timeRemaining && <div className="no-time-reamain"><span>שעת יציאה </span>{route.first_train.substring(0, 5)}</div>}
-            {timeRemaining && <div className="time-reamain"><div className="blink_me"><BsCircleFill/></div>{timeRemaining}</div>}
+            {timeRemaining && <div className="time-reamain"><div className="blink_me"><BsCircleFill /></div>{timeRemaining}</div>}
             {siri && <div className="siri">איחור {getTimeDiff()}</div>}
          </div>
 
